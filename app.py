fd47c1640d9845ec5ea754287b37b1b155c970dd
Rigved Sarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2025-02-24 16:16:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,10 +60,10 @@ def generate_invoice(customer_name, gst_number, contact_number, address, selecte
     current_date = datetime.now().strftime("%d-%m-%Y")
 
     # Sales Person
-    pdf.ln(5)
+    pdf.ln(1)
     pdf.set_font("Arial", 'B', 10)
     pdf.cell(0, 10, f"Sales Person: {employee_name}", ln=True, align='L')
-    pdf.ln(10)
+    pdf.ln(2)
 
     # Customer details
     pdf.set_font("Arial", 'B', 12)
